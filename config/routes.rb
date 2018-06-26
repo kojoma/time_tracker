e33ad2a21975f1e6c72a01f94c39b8fe791b51d1
kojoma <masaru.kojo@gmail.com>
2018-06-26 18:42:07 +0900

    add users index, show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
+  resources :users, :only => [:index, :show]
   resources :work_hours
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
