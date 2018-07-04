072248460726f685dc0be73b972b09304e5c53df
masaru.kojo <masaru.kojo@mixi.co.jp>
2018-07-04 17:48:02 +0900

    開発環境はsign upできる

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  devise :registerable if Rails.env.development?
 end
